db9f7ba71950cc78a0323104f0edd34d6417942e
tarik.onal <tarik.onal@csgb.gov.tr>
2023-03-27 17:28:25 +0300

    Add project files.

---
+++ NLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();  
+        }
+    }
+}
